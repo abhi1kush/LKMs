df9e6bbb8202b383d692b7f4a71368db1d1a180e
abhi <abhi1kush@gmail.com>
2015-03-28 13:29:04 +0530

    hang issues pending to solve

--- assg4/assg4.c
+++ assg4/assg4.c
@@ -16,15 +16,17 @@
 
 extern int (*context_counter)(struct task_struct *,struct task_struct *,long,int);
 extern int (* rq_size)(int,unsigned int,unsigned long);
+extern int (* per_pid_stats)(struct task_struct *);
 int pid,prio;
 
 //module_param(pid, int, 0);
 //module_param(prio, int, 0);
 
-static int sched_counter[4],per_pid_switch_counter[4],grqsize[4];
+static unsigned long  sched_counter[4],per_pid_switch_counter[4];
+static long grqsize[4];
 static int gcpu;
 static unsigned int entryy,exitt,total;
-
+static unsigned long migration,tot,switches[4];
 /***********************proc file section start *************************************************************/
 struct proc_dir_entry *assi4_proc_file;
 struct proc_dir_entry *assi4_proc_file2;
@@ -59,13 +61,14 @@ int proc_open2(struct inode *pinode, struct file *pfile)
 
 static int proc_readings(struct seq_file * sfile, void * any)
 {
-	seq_printf(sfile,"No of Reads: d\nNo of Writes: d");
+	seq_printf(sfile,": d\nNo of Writes: d");
 	return 0;
 }
 
 static int proc_readings2(struct seq_file * sfile, void * any)
 {
-	seq_printf(sfile,"No of Reads: d\nNo of Writes: d\n");
+	seq_printf(sfile,"#pid: %d No of CS: cpu0 %ld cpu1 %ld cpu %ld cpu %ld\n",pid,switches[0],switches[1],switches[2],switches[3]);
+	seq_printf(sfile,"$pid: %d No of CS: cpu0 %ld cpu1 %ld cpu %ld cpu %ld\n",pid,sched_counter[0],sched_counter[1],sched_counter[2],sched_counter[3]);
 	return 0;
 }
 /******************************** proc file section end **************************************************************/
@@ -91,6 +94,17 @@ int hk_rq_size(int cpu,unsigned int rqsize,unsigned long nr_switches)
 {
 	gcpu=cpu;
 	grqsize[cpu]=rqsize;
+	switches[cpu]=nr_switches;
+	return 0;
+}
+
+int hk_per_pid_stats(struct task_struct *prev)
+{	
+	if(prev->pid == pid)
+	{	
+		tot = prev->se.sum_exec_runtime;
+		migration = prev->se.nr_migrations;
+	}
 	return 0;
 }
 
@@ -114,8 +128,10 @@ static int init(void)
 		return -1;
 	}
 	printk(KERN_INFO"proc entry created \n");
+	// asgining hook pointer
 	context_counter = hk_context_counter;
 	rq_size=hk_rq_size;  	
+	per_pid_stats=hk_per_pid_stats;
 	printk(KERN_INFO "inside the %s function\n", __FUNCTION__);
   	return 0;
 }
@@ -124,6 +140,7 @@ static void  cleanup(void)
 {
   	rq_size=NULL;
   	context_counter=NULL;
+	per_pid_stats=NULL;
 	proc_remove(assi4_proc_file);
 	proc_remove(assi4_proc_file2);
   	printk(KERN_INFO "lkm removed\n");
