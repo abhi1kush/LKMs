08c772ea8384b1968383ad737ac2db2ab4935d81
abhi <abhi1kush@gmail.com>
2015-03-29 13:44:55 +0530

    push newrq

--- assg4/ba2.sh
+++ assg4/ba2.sh
@@ -1,9 +1,9 @@
-for i in {0..100}
-do
 sudo rmmod assg4
 sudo insmod assg4.ko
+for i in {0..100}
+do
 sleep 5
-cat /proc/my_proc_file2 | grep "*:" | cut -d" " -f 6,8,10,12 >> rqtemp1
+cat /proc/my_proc_file2 | grep "*:" | cut -d" " -f 6,8,10,12 >>newrq2
 done
 
 
